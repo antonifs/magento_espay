8edfb7a426622daa6d4acc1090b2dde36ea347ee
Antoni F. Setiawan <antoni.fs@gmail.com>
2017-03-08 17:04:13 +0700

    adding error log

--- app/code/local/Plus/Espaypaymentmethod/controllers/PaymentController.php
+++ app/code/local/Plus/Espaypaymentmethod/controllers/PaymentController.php
@@ -76,7 +76,7 @@ class Plus_Espaypaymentmethod_PaymentController extends Mage_Core_Controller_Fro
         $transfer_bank = "Espay";
 
         $url = "http://vendor.tinkerlust.com/api_paymentconfirm.php?payment_sd=yes&name=". urlencode($name) . "&email=". urlencode($email) . "&order_no=". urlencode($order_no)."&total=". urlencode($total) ."&transfer_no=" . urlencode($transfer_no) . "&transfer_bank=".urlencode($transfer_bank);
-        Mage::log(print_r($da, 1), null, 'espay_querystring.log');
+        Mage::log(print_r($url, 1), null, 'espay_querystring.log');
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -85,7 +85,8 @@ class Plus_Espaypaymentmethod_PaymentController extends Mage_Core_Controller_Fro
         curl_setopt($curl, CURLOPT_POST, 1);
         $resp = curl_exec($curl);
         curl_close($curl);        
-        // end of sending payment information
+        // end
+
 
         $this->loadLayout();
         $block = $this->getLayout()->createBlock('Mage_Core_Block_Template', 'espaypaymentmethod', array('template' => 'espaypaymentmethod/redirect.phtml'));
